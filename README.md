6f8e6a605e113a1de55a6aadf32521283087ca06
Gary Spillman <garyspillman@Garys-MBP.attlocal.net>
2020-04-10 17:58:20 -0500

    Initial commit of my changes to own fork of this proj.

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
-# viz_dstat
-Visualize dstat results on a web page.  Can optionally launch dstat and monitor results in real time.
+# viz_dstat_lite
+Visualize dstat results on a web page.
+
+Modified scripts from:
+https://github.com/jschaub30/viz_dstat
 
 #Requires
-- [dstat](http://dag.wiee.rs/home-made/dstat/)
-- [python](https://www.python.org/)
+- [python 3](https://www.python.org/)
+- Assumes you have generated a series of dstat csv reports 
 
 Also uses [Dygraphs](http://dygraphs.com/) for javascript charts (included)
 
 ##Visualize existing file
 ```
-$ dstat --time -v --net --output example.csv 1 10
-$ ./viz_dstat.sh example.csv
-```
-
-##Measure and visualize
-```
-$ ./viz_dstat.sh
+$ ./viz_dstat.sh report1.csv,report2.csv,report3.csv,...etc
 ```
 
 Then point your browser to http://localhost:12121
+
+Each report can be viewed independently outside of the tabbed interface by going to: 
+http://localhost:12121/index<insert report id>.html
+
+To Dos:
+
+- Modify script to dynamially append tabs for reports beyond initial 3
+- Dockerize this so we can put it into a CI instance
--- viz_dstat.sh
+++ viz_dstat.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ksh
 # Launch webserver and visualize dstat measurement in a browser
 # Create dstat file using this format
 #  $ dstat --time -v --net --output [DSTAT_CSV] [DELAY_SEC]
@@ -8,44 +8,44 @@
 # If called with no arguments, will launch dstat measurement
 #  $ ./viz_dstat.sh
 
-CSV_FN=$1
-HTML_DIR=html.$(date +"%Y%m%d-%H%M%S")
+
+HTML_DIR=dstat-report-$(date +"%Y%m%d-%H%M%S")
 mkdir -p $HTML_DIR
 cp -r js $HTML_DIR/.
 cp style.css $HTML_DIR/.
 echo Working directory is $HTML_DIR
 
+CSV_FN=$1
+set -A A_FILES $( echo "$CSV_FN" | sed 's/,/ /g' )
+i=0
+while [ -n "${A_FILES[i]}" ]
+  do
+     echo "Visualizing file ${A_FILES[i]}"
+     cp ${A_FILES[i]} $HTML_DIR/.
+     #cat index.html | sed "s/dstat.csv/${A_FILES[i]}/g" > $HTML_DIR/index.html
+     cat index.html | sed "s/dstat.csv/${A_FILES[i]}/g" > $HTML_DIR/index.html
+     cat $HTML_DIR/index.html | sed "s/dstat-graph/${A_FILES[i]}-report/g" > $HTML_DIR/index$i.html
+     (( i = i + 1 ))
+  done
+echo "Files copied"
+
+cp tabbed_index.html $HTML_DIR/
+cd $HTML_DIR
+rm index.html
+mv tabbed_index.html index.html
+
 kill_server() {
-	echo 
-  [ $DSTAT_PID ] && echo "Stopping dstat measurement" && kill $DSTAT_PID > /dev/null
+	echo
+ [ $DSTAT_PID ] && echo "Stopping dstat measurement" && kill $DSTAT_PID > /dev/null
   echo "Stopping web server"
 	kill $SRV_PID 2> /dev/null
 }
 trap 'kill_server' SIGTERM SIGINT # Kill measurement when webserver is killed
 
-if [ $# -lt 1 ]
-then
-  # Launch dstat measurement
-  echo Starting dstat for 10 minutes using this command:
-  echo "dstat --time -v --net --output dstat.csv 1 600"
-  dstat --time -v --net --output $HTML_DIR/dstat.csv 1 600 > /dev/null &
-  DSTAT_PID=$!
-  ARG="and dstat measurement"
-  cat index.html | sed "s/refresh_page = false/refresh_page = true/" | \
-     sed "s/Visualize dstat measurement/Visualize dstat measurement on $(hostname)/" \
-     > $HTML_DIR/index.html
-else
-  echo Visualizing file $1
-  cp $1 $HTML_DIR/.
-  cat index.html | sed "s/dstat.csv/$1/g" > $HTML_DIR/index.html
-fi
-  
-cd $HTML_DIR
-
 echo Starting web server--point your browser to http://localhost:12121
-echo 
 python -m SimpleHTTPServer 12121 2> http_log&
 SRV_PID=$!
 sleep 0.5
-read -p "Press return to stop web server $ARG"
+echo "Press a key to stop web server"
+read text
 kill_server
